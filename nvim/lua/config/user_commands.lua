adb275977b15a7837d33bcb1893fc2a06e923f15
lgivhan <youngturkay@gmail.com>
2026-01-26 16:12:51 -0500

    Add custom git sync command for quick commits

--- nvim/lua/config/user_commands.lua
+++ nvim/lua/config/user_commands.lua
@@ -1,3 +1,17 @@
 vim.api.nvim_create_user_command('H', function(table)
   vim.cmd('help ' .. table.args .. ' | only')
 end, { desc = 'Open [H]elp in new tab', nargs = 1 })
+
+-- Custom Git Sync (gs) command
+-- Usage: :Gs "your commit message"
+vim.api.nvim_create_user_command('Gs', function(opts)
+    local msg = opts.args
+    if msg == "" then
+        print("Error: Commit message required!")
+        return
+    end
+
+    -- The '!' runs it in your system shell
+    -- '&&' ensures each step succeeds before moving to the next
+    vim.cmd('!git add . && git commit -m "' .. msg .. '" && git push')
+end, { nargs = 1 })
